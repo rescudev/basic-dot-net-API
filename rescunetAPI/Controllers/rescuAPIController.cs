5ca7ab69dec40c2ab66b41f3fef8d1d54b804963
rescudev <rescuderolirio@gmail.com>
2023-07-31 21:09:41 +0200

    REST basic structure

--- rescunetAPI/Controllers/rescuAPIController.cs
+++ rescunetAPI/Controllers/rescuAPIController.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using Microsoft.AspNetCore.Mvc;
+using rescunetAPI.Models;
 using rescunetAPI.Models.DTO;
 using rescunetAPI.Structures;
 
@@ -10,11 +11,77 @@ namespace rescunetAPI.Controllers
     [ApiController]
     public class rescuAPIController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<CustomerDTO> GetCustomers() 
+        [HttpGet(Name = "GetCustomers")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult <IEnumerable<CustomerDTO>> GetCustomers() 
         {
-            return CustomerST.customers;
+            return Ok(CustomerST.customers);
+        }
+
+        [HttpGet("{IdCustomer:int}", Name = "GetCustomer")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CustomerDTO> GetCustomer(int IdCustomer)
+        {
+            if (IdCustomer <= 0) 
+            {
+                return BadRequest();
+            }
+            CustomerDTO customer = CustomerST.customers.Find(x => x.IdCustomer == IdCustomer);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            return Ok(customer);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<CustomerDTO> RegisterCustomer([FromBody]CustomerDTO customerDTO)
+        {
+            if(CustomerST.customers.FirstOrDefault(x => x.UserName == customerDTO.UserName) != null)
+            {
+                ModelState.AddModelError("DuplicatedError", "The UserName is duplicated.");
+                return BadRequest(ModelState);
+            }
+            
+            if (customerDTO == null)
+            {
+                return BadRequest(customerDTO);
+            }
+            if(customerDTO.IdCustomer > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            customerDTO.IdCustomer = CustomerST.customers.OrderBy(x => x.IdCustomer).Last().IdCustomer+1;
+            CustomerST.customers.Add(customerDTO);
+
+            return CreatedAtRoute("GetCustomer", new { idCustomer = customerDTO.IdCustomer }, customerDTO);
+        }
+
+        [HttpDelete("{IdCustomer:int}", Name = "DeleteCustomer")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult DeleteCustomer(int IdCustomer)
+        {
+            if (IdCustomer <= 0)
+            {
+                return BadRequest();
+            }
+            CustomerDTO customer = CustomerST.customers.Find(x => x.IdCustomer == IdCustomer);
+            if (customer == null)
+            {
+                return NotFound();
+            }
+            CustomerST.customers.Remove(customer);
+            return NoContent();
         }
 
     }
+
 }
--- rescunetAPI/Models/DTO/CustomerDTO.cs
+++ rescunetAPI/Models/DTO/CustomerDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace rescunetAPI.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace rescunetAPI.Models.DTO
 {
     public class CustomerDTO
     {
         public int IdCustomer { get; set; }
+        [Required]
+        [MaxLength(32)]
         public string UserName { get; set; }
         public int Balance { get; set; }
         public DateTime TimeStamp { get; set; }
